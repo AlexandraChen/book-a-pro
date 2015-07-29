aabb8faedbd086d90b5547a9c76984db8f470980
Alexandra Chen <alechen04@gmail.com>
2015-07-29 09:41:19 +0200

    vertical alignment to pics

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,13 @@ class User < ActiveRecord::Base
 
   has_one :professional, dependent: :destroy
 
-  has_attached_file :prof_pic, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "150x150#{}"  }
+  has_attached_file :prof_pic,
+  	:styles => { 
+	  	:medium => "300x300>",
+	  	:thumb => "100x100>", 
+	  	:small => "150x150#"  
+  	}
+  	
  	validates_attachment_content_type :prof_pic, :content_type => /\Aimage\/.*\Z/
 
 end
