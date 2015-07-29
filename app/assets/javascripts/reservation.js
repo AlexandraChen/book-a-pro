fbdd33c618fc34f8cf9c7f8fd4d4158b3abda42f
Alexandra Chen <alechen04@gmail.com>
2015-07-29 15:29:38 +0200

    add reservation on click

--- app/assets/javascripts/reservation.js
+++ app/assets/javascripts/reservation.js
@@ -1,11 +1,15 @@
 $(function () {
-	$("#reservation").on("click",function(){
+	$("#make-reservation").on("click",function(){
 		$(this).hide()
 		$(".reservation-form").show()
 	})
 
 	$("#show-reservations").on("click", function(){
 		$(".my-reservations").show()
+	})
+
+	$(".view-reservation").on("click", function(){
+		$(".reservation").show()
 	})
 
 })
