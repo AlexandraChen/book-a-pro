d008e4a6bb8e5beab9ae53e584571c6ee545bdef
Alexandra Chen <alechen04@gmail.com>
2015-07-31 12:36:15 +0200

    style: fading panels

---
+++ app/assets/javascripts/panel.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	var currentBackground = 0;
+	var backgrounds = [
+		'/assets/sites/boxing.jpg',
+		'/assets/sites/tutoring.jpg',
+		'/assets/sites/dentist.jpg',
+		'/assets/sites/chef.jpg',
+		'/assets/sites/tutoring_kids.jpg',
+		'/assets/sites/boxing.jpg'
+	]
+
+	function changeBackground() {
+	  currentBackground++;
+	  if(currentBackground > backgrounds.length-1) currentBackground = 0;
+		$('.main-panel').css({
+	      'background-image' : "url('" + backgrounds[currentBackground] + "')"
+	  });
+	  setTimeout(changeBackground, 3000);
+	}
+	  setTimeout(changeBackground, 3000);     
+});
+
+	
