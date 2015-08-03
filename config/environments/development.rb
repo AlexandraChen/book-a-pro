03c0b75b95275fc03d8706aee5194bb4d87bebff
Alexandra Chen <alechen04@gmail.com>
2015-08-03 15:33:45 +0200

    paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,11 +47,11 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/"
   # #paperclip aws config
   # config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  #   storage: :s3,
+  #   s3_credentials: {
+  #     bucket: ENV['S3_BUCKET_NAME'],
+  #     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
 end
