07069b04605f50e9ef40bec8d064564ed54d274f
Alexandra Chen <alechen04@gmail.com>
2015-08-03 12:21:58 +0200

    comment paper clip aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,14 +44,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
 
   #Images active recorde paperclip
-  Paperclip.options[:command_path] = "/usr/local/bin/"
-  #paperclip aws config
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  # Paperclip.options[:command_path] = "/usr/local/bin/"
+  # #paperclip aws config
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_credentials: {
+  #     bucket: ENV['S3_BUCKET_NAME'],
+  #     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
 end
