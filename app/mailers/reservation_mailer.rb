1ea51ed2d4183d051e29e05d4a63cdf8b089e03b
Alexandra Chen <alechen04@gmail.com>
2015-08-04 11:41:01 +0200

    more info on reservation email

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,11 +3,11 @@ class ReservationMailer < ApplicationMailer
 
 	def user_reservation_email(reservation)
 		@reservation = reservation
-		mail(to: @reservation.user.email, subject: "#{@reservation.user.name}, here is your new reservation", template_name: "user_reservation")
+		mail(to: @reservation.user.email, subject: "#{@reservation.user.name}, here is your new Book-a-Pro reservation", template_name: "user_reservation")
 	end
 
 	def pro_reservation_email(reservation)
 		@reservation = reservation
-		mail(to: @reservation.professional.user.email, subject: "#{@reservation.professional.user.name}, here is your new reservation", template_name: "pro_reservation")
+		mail(to: @reservation.professional.user.email, subject: "#{@reservation.professional.user.name}, #{@reservation.user.name} requested a Book-a-Pro reservation", template_name: "pro_reservation")
 	end
 end
