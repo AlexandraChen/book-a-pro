3c79f21b47c0adc88d270ab03bf2da813f716740
Alexandra Chen <alechen04@gmail.com>
2015-12-04 13:10:39 -0500

    Added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_one :professional, dependent: :destroy
   has_many :reservations, dependent: :destroy
   has_many :reviews
-  validates :email, presence: true, length: {maximum: 250}
+  validates :email, presence: true, format: { with: /.+@.+\..+/i }
   validates :name, presence: true #space validation
   # validates :last_name, presence: true, length: {maximum: 30}
   has_attached_file :prof_pic,
